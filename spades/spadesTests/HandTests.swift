ca12f866187b29f62a1d8704e685ff084e1de1e6
Jennifer Loecker <jenniferloecker@Jennifers-MacBook-Pro.local>
2017-01-16 21:04:43 -0600

    Added a test method for the ace score calculation

--- spades/spadesTests/HandTests.swift
+++ spades/spadesTests/HandTests.swift
@@ -54,4 +54,37 @@ class HandTests: XCTestCase {
         let bid = hand.bid()
         XCTAssertEqual(bid, 13)
     }
+    
+    func testAceScore() {
+        let hand1 = Hand()
+        hand1.cards.append(Card(rank: .Ace, suit: .Spade))
+        hand1.cards.append(Card(rank: .King, suit: .Spade))
+        XCTAssertEqual(hand1.aceScore(), 2)
+        
+        let hand2 = Hand()
+        hand2.cards.append(Card(rank: .King, suit: .Spade))
+        hand2.cards.append(Card(rank: .Ten, suit: .Spade))
+        hand2.cards.append(Card(rank: .Two, suit: .Spade))
+        XCTAssertEqual(hand2.aceScore(), 1)
+        
+        let hand3 = Hand()
+        hand3.cards.append(Card(rank: .King, suit: .Spade))
+        hand3.cards.append(Card(rank: .Nine, suit: .Spade))
+        hand3.cards.append(Card(rank: .Six, suit: .Spade))
+        hand3.cards.append(Card(rank: .Four, suit: .Spade))
+        XCTAssertEqual(hand3.aceScore(), 2)
+        
+        let hand4 = Hand()
+        hand4.cards.append(Card(rank: .Nine, suit: .Spade))
+        hand4.cards.append(Card(rank: .Eight, suit: .Spade))
+        hand4.cards.append(Card(rank: .Six, suit: .Spade))
+        hand4.cards.append(Card(rank: .Three, suit: .Spade))
+        XCTAssertEqual(hand4.aceScore(), 1)
+        
+        let hand5 = Hand()
+        hand5.cards.append(Card(rank: .Ten, suit: .Spade))
+        hand5.cards.append(Card(rank: .Five, suit: .Spade))
+        hand5.cards.append(Card(rank: .Three, suit: .Spade))
+        XCTAssertEqual(hand5.aceScore(), 0)
+    }
 }
